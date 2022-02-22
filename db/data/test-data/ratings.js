3cdb67142cdf1b69dd9f5434736bb770426ec275
MarshmallowFlump <lfhlord@gmail.com>
2022-02-22 15:26:54 +0000

    ratings test data added

--- db/data/test-data/ratings.js
+++ db/data/test-data/ratings.js
@@ -1,12 +1,82 @@
 module.exports = [
   {
-    task_id: 1,
-    rating: 9,
-    comment: "Good",
-  },
-  {
-    task_id: 2,
-    rating: 10,
-    comment: "Great",
-  },
-];
+  "task_id": 1,
+  "rating": 7,
+  "comment": "nulla"
+}, {
+  "task_id": 2,
+  "rating": 2,
+  "comment": "curabitur"
+}, {
+  "task_id": 3,
+  "rating": 5,
+  "comment": "ac"
+}, {
+  "task_id": 4,
+  "rating": 2,
+  "comment": "proin"
+}, {
+  "task_id": 5,
+  "rating": 2,
+  "comment": "ante"
+}, {
+  "task_id": 6,
+  "rating": 8,
+  "comment": "etiam"
+}, {
+  "task_id": 7,
+  "rating": 7,
+  "comment": "sit"
+}, {
+  "task_id": 8,
+  "rating": 5,
+  "comment": "magna"
+}, {
+  "task_id": 9,
+  "rating": 2,
+  "comment": "libero"
+}, {
+  "task_id": 10,
+  "rating": 7,
+  "comment": "libero"
+}, {
+  "task_id": 11,
+  "rating": 9,
+  "comment": "convallis"
+}, {
+  "task_id": 12,
+  "rating": 8,
+  "comment": "nibh"
+}, {
+  "task_id": 13,
+  "rating": 9,
+  "comment": "in"
+}, {
+  "task_id": 14,
+  "rating": 9,
+  "comment": "eleifend"
+}, {
+  "task_id": 15,
+  "rating": 6,
+  "comment": "dignissim"
+}, {
+  "task_id": 16,
+  "rating": 10,
+  "comment": "molestie"
+}, {
+  "task_id": 17,
+  "rating": 4,
+  "comment": "faucibus"
+}, {
+  "task_id": 18,
+  "rating": 10,
+  "comment": "mattis"
+}, {
+  "task_id": 19,
+  "rating": 8,
+  "comment": "ligula"
+}, {
+  "task_id": 20,
+  "rating": 2,
+  "comment": "condimentum"
+}];
