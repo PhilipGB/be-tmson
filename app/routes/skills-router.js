e914bc2d708ae53aa6df0885a42207a8aa8ab21a
Oscar <oj_24@hotmail.com>
2022-02-23 13:44:47 +0000

    skills router

--- app/routes/skills-router.js
+++ app/routes/skills-router.js
@@ -1,22 +1,14 @@
-const skillsRouter = require("express").Router();
-// const {} = require("../controllers/skills-controllers.js");
+const skillsRouter = require('express').Router();
+const {
+  getAllSkills,
+  getSkillsByCategory,
+  postNewSkill,
+} = require('../controllers/skills-controllers.js');
 
-skillsRouter
-  .route("/")
-  .get((req, res) => {
-    res.status(200).send("All OK from POST /api/skills");
-  })
-  .post((req, res) => {
-    res.status(200).send("All OK from POST /api/skills");
-  });
-
-skillsRouter
-  .route("/:skill")
-  .get((req, res) => {
-    res.status(200).send("All OK from GET /api/skills/:skill");
-  })
-  .patch((req, res) => {
-    res.status(200).send("All OK from PATCH /api/skills/:skill");
-  });
+skillsRouter.route('/').get(getAllSkills).post(postNewSkill);
+skillsRouter.route('/:category').get(getSkillsByCategory);
+skillsRouter.patch((req, res) => {
+  res.status(200).send('All OK from PATCH /api/skills/:category');
+});
 
 module.exports = skillsRouter;
