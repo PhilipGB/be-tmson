b9d806731998ff604a516c10bdabec906eb0361e
Philip Burgess <philipgrahamburgess@gmail.com>
2022-02-23 10:32:27 +0000

    token owner_id's limited to 1-20

--- db/data/test-data/tokens.js
+++ db/data/test-data/tokens.js
@@ -1,102 +1,122 @@
 module.exports = [
   {
-  "token_id": 1,
-  "generated_date": "2022-02-18 06:56:21 UTC",
-  "owner_id": 7,
-  "minter_id": 1
-}, {
-  "token_id": 2,
-  "generated_date": "2022-02-18 07:56:21 UTC",
-  "owner_id": 14,
-  "minter_id": 1
-}, {
-  "token_id": 3,
-  "generated_date": "2022-02-18 08:56:21 UTC",
-  "owner_id": 17,
-  "minter_id": 1
-}, {
-  "token_id": 4,
-  "generated_date": "2022-02-18 09:56:21 UTC",
-  "owner_id": 13,
-  "minter_id": 1
-}, {
-  "token_id": 5,
-  "generated_date": "2022-02-18 10:56:21 UTC",
-  "owner_id": 25,
-  "minter_id": 1
-}, {
-  "token_id": 6,
-  "generated_date": "2022-02-18 11:56:21 UTC",
-  "owner_id": 23,
-  "minter_id": 1
-}, {
-  "token_id": 7,
-  "generated_date": "2022-02-18 12:56:21 UTC",
-  "owner_id": 4,
-  "minter_id": 1
-}, {
-  "token_id": 8,
-  "generated_date": "2022-02-18 13:56:21 UTC",
-  "owner_id": 3,
-  "minter_id": 1
-}, {
-  "token_id": 9,
-  "generated_date": "2022-02-18 14:56:21 UTC",
-  "owner_id": 11,
-  "minter_id": 1
-}, {
-  "token_id": 10,
-  "generated_date": "2022-02-18 15:56:21 UTC",
-  "owner_id": 15,
-  "minter_id": 1
-}, {
-  "token_id": 11,
-  "generated_date": "2022-02-18 16:56:21 UTC",
-  "owner_id": 19,
-  "minter_id": 1
-}, {
-  "token_id": 12,
-  "generated_date": "2022-02-18 17:56:21 UTC",
-  "owner_id": 16,
-  "minter_id": 1
-}, {
-  "token_id": 13,
-  "generated_date": "2022-02-18 18:56:21 UTC",
-  "owner_id": 18,
-  "minter_id": 1
-}, {
-  "token_id": 14,
-  "generated_date": "2022-02-18 19:56:21 UTC",
-  "owner_id": 5,
-  "minter_id": 1
-}, {
-  "token_id": 15,
-  "generated_date": "2022-02-18 20:56:21 UTC",
-  "owner_id": 23,
-  "minter_id": 1
-}, {
-  "token_id": 16,
-  "generated_date": "2022-02-18 21:56:21 UTC",
-  "owner_id": 17,
-  "minter_id": 1
-}, {
-  "token_id": 17,
-  "generated_date": "2022-02-18 22:56:21 UTC",
-  "owner_id": 19,
-  "minter_id": 1
-}, {
-  "token_id": 18,
-  "generated_date": "2022-02-18 23:56:21 UTC",
-  "owner_id": 18,
-  "minter_id": 1
-}, {
-  "token_id": 19,
-  "generated_date": "2022-02-19 00:56:21 UTC",
-  "owner_id": 8,
-  "minter_id": 1
-}, {
-  "token_id": 20,
-  "generated_date": "2022-02-19 01:56:21 UTC",
-  "owner_id": 12,
-  "minter_id": 1
-}];
+    token_id: 1,
+    generated_date: "2022-02-18 06:56:21 UTC",
+    owner_id: 7,
+    minter_id: 1,
+  },
+  {
+    token_id: 2,
+    generated_date: "2022-02-18 07:56:21 UTC",
+    owner_id: 14,
+    minter_id: 1,
+  },
+  {
+    token_id: 3,
+    generated_date: "2022-02-18 08:56:21 UTC",
+    owner_id: 17,
+    minter_id: 1,
+  },
+  {
+    token_id: 4,
+    generated_date: "2022-02-18 09:56:21 UTC",
+    owner_id: 13,
+    minter_id: 1,
+  },
+  {
+    token_id: 5,
+    generated_date: "2022-02-18 10:56:21 UTC",
+    owner_id: 20,
+    minter_id: 1,
+  },
+  {
+    token_id: 6,
+    generated_date: "2022-02-18 11:56:21 UTC",
+    owner_id: 16,
+    minter_id: 1,
+  },
+  {
+    token_id: 7,
+    generated_date: "2022-02-18 12:56:21 UTC",
+    owner_id: 4,
+    minter_id: 1,
+  },
+  {
+    token_id: 8,
+    generated_date: "2022-02-18 13:56:21 UTC",
+    owner_id: 3,
+    minter_id: 1,
+  },
+  {
+    token_id: 9,
+    generated_date: "2022-02-18 14:56:21 UTC",
+    owner_id: 11,
+    minter_id: 1,
+  },
+  {
+    token_id: 10,
+    generated_date: "2022-02-18 15:56:21 UTC",
+    owner_id: 15,
+    minter_id: 1,
+  },
+  {
+    token_id: 11,
+    generated_date: "2022-02-18 16:56:21 UTC",
+    owner_id: 19,
+    minter_id: 1,
+  },
+  {
+    token_id: 12,
+    generated_date: "2022-02-18 17:56:21 UTC",
+    owner_id: 16,
+    minter_id: 1,
+  },
+  {
+    token_id: 13,
+    generated_date: "2022-02-18 18:56:21 UTC",
+    owner_id: 18,
+    minter_id: 1,
+  },
+  {
+    token_id: 14,
+    generated_date: "2022-02-18 19:56:21 UTC",
+    owner_id: 5,
+    minter_id: 1,
+  },
+  {
+    token_id: 15,
+    generated_date: "2022-02-18 20:56:21 UTC",
+    owner_id: 3,
+    minter_id: 1,
+  },
+  {
+    token_id: 16,
+    generated_date: "2022-02-18 21:56:21 UTC",
+    owner_id: 17,
+    minter_id: 1,
+  },
+  {
+    token_id: 17,
+    generated_date: "2022-02-18 22:56:21 UTC",
+    owner_id: 19,
+    minter_id: 1,
+  },
+  {
+    token_id: 18,
+    generated_date: "2022-02-18 23:56:21 UTC",
+    owner_id: 18,
+    minter_id: 1,
+  },
+  {
+    token_id: 19,
+    generated_date: "2022-02-19 00:56:21 UTC",
+    owner_id: 8,
+    minter_id: 1,
+  },
+  {
+    token_id: 20,
+    generated_date: "2022-02-19 01:56:21 UTC",
+    owner_id: 12,
+    minter_id: 1,
+  },
+];
