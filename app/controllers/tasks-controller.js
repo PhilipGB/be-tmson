251bfa30bcb928802f30e851e6e81c8831d81c1a
Oscar <oj_24@hotmail.com>
2022-02-28 17:22:57 +0000

    added skill data to fetchtaskbyId

--- app/controllers/tasks-controller.js
+++ app/controllers/tasks-controller.js
@@ -1,53 +1,54 @@
 const {
-	fetchTasks,
-	fetchTaskById,
-	updateTaskById,
-	writeNewTask,
-	eraseTask,
+  fetchTasks,
+  fetchTaskById,
+  updateTaskById,
+  writeNewTask,
+  eraseTask,
 } = require('../models/tasks.model');
 
 exports.getTasks = (req, res, next) => {
-	const { sort_by } = req.query;
-	const { order } = req.query;
-	fetchTasks(sort_by, order)
-		.then((tasks) => {
-			res.status(200).send({ tasks });
-		})
-		.catch(next);
+  const { sort_by } = req.query;
+  const { order } = req.query;
+  fetchTasks(sort_by, order)
+    .then((tasks) => {
+      res.status(200).send({ tasks });
+    })
+    .catch(next);
 };
 
 exports.getTaskById = (req, res, next) => {
-	const { task_id } = req.params;
-	fetchTaskById(task_id)
-		.then((task) => {
-			res.status(200).send({ task });
-		})
-		.catch(next);
+  const { task_id } = req.params;
+  fetchTaskById(task_id)
+    .then((task) => {
+      console.log(task);
+      res.status(200).send({ task });
+    })
+    .catch(next);
 };
 
 exports.patchTaskById = (req, res, next) => {
-	const { task_id } = req.params;
-	const updatedTaskBody = req.body;
-	updateTaskById(updatedTaskBody, task_id)
-		.then((task) => {
-			res.status(200).send({ task });
-		})
-		.catch(next);
+  const { task_id } = req.params;
+  const updatedTaskBody = req.body;
+  updateTaskById(updatedTaskBody, task_id)
+    .then((task) => {
+      res.status(200).send({ task });
+    })
+    .catch(next);
 };
 
 exports.postTask = (req, res, next) => {
-	writeNewTask(req.body)
-		.then((task) => {
-			res.status(201).send({ task });
-		})
-		.catch(next);
+  writeNewTask(req.body)
+    .then((task) => {
+      res.status(201).send({ task });
+    })
+    .catch(next);
 };
 
 exports.deleteTask = (req, res, next) => {
-	const { task_id } = req.params;
-	eraseTask(task_id)
-		.then(() => {
-			res.status(204).send({ msg: 'No Content' });
-		})
-		.catch(next);
+  const { task_id } = req.params;
+  eraseTask(task_id)
+    .then(() => {
+      res.status(204).send({ msg: 'No Content' });
+    })
+    .catch(next);
 };
